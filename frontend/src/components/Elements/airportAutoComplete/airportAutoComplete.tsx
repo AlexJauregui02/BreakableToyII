1e449c63f64f1e0f9d2edfbdcb294f9bdb5f751c
AlexJauregui02 <alejandro.jauregui@encora.com>
2025-09-03 09:51:02 -0600

    .

--- frontend/src/components/Elements/airportAutoComplete/airportAutoComplete.tsx
+++ frontend/src/components/Elements/airportAutoComplete/airportAutoComplete.tsx
@@ -1,8 +1,9 @@
-import AsyncSelect from "react-select/async";
-import { searchAirportsByKeyword } from "@/api/services/flightSearchService";
-import type { IataCodeAirportSearchResponse } from "@/types/iataCodeSearch";
-import type { SelectOption } from "@/types/option";
-import type { AirportAutoCompleProps } from "@/types/props";
+import AsyncSelect from 'react-select/async';
+import { searchAirportsByKeyword } from '@/api/services/flightSearchService';
+import type { IataCodeAirportSearchResponse } from '@/types/iataCodeSearch';
+import type { SelectOption } from '@/types/option';
+import type { AirportAutoCompleProps } from '@/types/props';
+
 
 export function AirportAutocomplete({ onChange, defaultValue }: AirportAutoCompleProps) {
   const loadOptions = async (inputValue: string): Promise<SelectOption[]> => {
@@ -10,9 +11,9 @@ export function AirportAutocomplete({ onChange, defaultValue }: AirportAutoCompl
 
     const results: IataCodeAirportSearchResponse[] = await searchAirportsByKeyword(inputValue);
 
-    return results.map((item) => ({
+    return results.map(item => ({
       label: `${item.name} (${item.iataCode})`,
-      value: item.iataCode,
+      value: item.iataCode
     }));
   };
 
@@ -25,9 +26,9 @@ export function AirportAutocomplete({ onChange, defaultValue }: AirportAutoCompl
       placeholder={"Search airport..."}
       unstyled
       classNames={{
-        control: () => "mt-1 px-2 rounded-sm border border-gray-400 text-sm shadow-sm",
-        option: () => "bg-white rounded-sm pl-2 py-1",
-        menu: () => "bg-white border border-gray-400 rounded-sm",
+        control: () => 'mt-1 px-2 rounded-sm border border-gray-400 text-sm shadow-sm',
+        option: () => 'bg-white rounded-sm pl-2 py-1',
+        menu: () => 'bg-white border border-gray-400 rounded-sm'
       }}
     />
   );
